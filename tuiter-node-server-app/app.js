52eba4a2d91952f3ad09e5872d92f426570b410c
Alice Liu <liuiman@126.com>
2023-06-12 11:48:34 +0800

    Changed origin path

--- tuiter-node-server-app/app.js
+++ tuiter-node-server-app/app.js
@@ -8,6 +8,8 @@ import session from "express-session"; // import new server session library
 import AuthController from "./users/auth-controller.js";
 
 const app = express();
+const port = process.env.PORT || 4000;
+
 app.use( // configure server session
          session({
                      secret: "any string",
@@ -17,24 +19,33 @@ app.use( // configure server session
 );
 // cors - cross-origin resource sharing - establish rules by which resources can be shared
 // across domains
+// app.use((req, res, next) => {
+//     const allowedOrigins = ["http://localhost:3000", "https://musical-duckanoo-05ddab.netlify.app/"];
+//     const origin = req.headers.origin;
+//
+//     if (allowedOrigins.includes(origin)) {
+//         res.header("Access-Control-Allow-Origin", origin);
+//     }
+//
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, PATCH, OPTIONS");
+//     res.header("Access-Control-Allow-Credentials", "true");
+//     next();
+// });
+
 app.use((req, res, next) => {
-    const allowedOrigins = ["http://localhost:3000", "https://musical-duckanoo-05ddab.netlify.app/"];
-    const origin = req.headers.origin;
-
-    if (allowedOrigins.includes(origin)) {
-        res.header("Access-Control-Allow-Origin", origin);
-    }
-
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, PATCH, OPTIONS");
-    res.header("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Origin", "https://musical-duckanoo-05ddab.netlify.app");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
     next();
 });
+
 app.use(express.json()); // parse JSON from HTTP request body because express does not know how
 // to extract data from an HTTP body. Express defines a JSON middleware to parse data from the
 // body. All requests will first go through this middleware parsing the HTTP body into a JSON
 // object added to the requested object in a new body property that later HTTP handlers can access.
-const port = process.env.PORT || 4000;
 TuitsController(app);
 HelloController(app);
 UserController(app);
