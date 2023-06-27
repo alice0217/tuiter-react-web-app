bb40122d2f5db066c735842994dbeac82b9abc6c
Alice Liu <liuiman@126.com>
2023-06-27 19:29:18 +0800

    fixed path

--- src/tuiter/services/tuits-service.js
+++ src/tuiter/services/tuits-service.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const TUITS_API = "http://localhost:4000/api/tuits"; // tuits server
-// const API_BASE = process.env.REACT_APP_API_BASE;
-// const TUITS_API = `${API_BASE}/tuits`;
+// const TUITS_API = "http://localhost:4000/api/tuits"; // tuits server
+const API_BASE = process.env.REACT_APP_API_BASE;
+const TUITS_API = `${API_BASE}/tuits`;
 
 // returns new tuit's data
 export const createTuit = async (tuit) => {
